098024dc1b4ab4082478c1a847d5d31ac75c44d6
Diego Fernando Caviedes <diego_1996.c.c@hotmail.com>
2022-02-14 23:34:09 -0500

    trabajando con metodo includes para arrays y cadenas

---
+++ server/include.js
@@ -0,0 +1,7 @@
+//Includes: Funciona para arrays y string
+const pets = ['cat', 'dog', 'bat'];
+
+const rta = pets.includes('dog');
+console.log('rta',rta)
+
+console.log('other',[1, 2, 3].includes(2));
