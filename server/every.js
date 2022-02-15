2bec4bd93b17ca05a2ddfb3fb0c2d5fd5b9e2ccc
Diego Fernando Caviedes <diego_1996.c.c@hotmail.com>
2022-02-14 22:51:53 -0500

    trabajando con metodo evey

---
+++ server/every.js
@@ -0,0 +1,36 @@
+//Todos cumplen con la condicion
+const numbers = [1, 30, 19, 29, 10, 13];
+
+
+const rta = numbers.every(item => item <= 40);
+console.log('rta', rta);
+
+//other example
+
+const team = [
+    {
+      name: "Nicolas",
+      age: 12,
+    },
+    {
+      name: "Andrea",
+      age: 8,
+    },
+    {
+      name: "Zulema",
+      age: 2,
+    },
+    {
+      name: "Santiago",
+      age: 18
+    },
+  ];
+
+const reto = team.every(({age}) => age <= 15);
+console.log('reto', reto);
+
+const admision = team => {
+    return team.every(player => player.age < 15)
+}
+
+console.log(admision(team));
