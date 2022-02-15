f43fd3423244c5f5e8632119e5ada0fab9ae19c1
Diego Fernando Caviedes <diego_1996.c.c@hotmail.com>
2022-02-14 20:11:33 -0500

    ajsute ejercicio de meotdo some

--- server/07-some.js
+++ server/07-some.js
@@ -1,44 +1,18 @@
 const numbers = [1, 2, 3, 4];
-//almenos si alguna
-const rta = numbers.some((item) => item % 2 === 0); //return boolean
-console.log("rtar", ta);
-/* --------------------------------------------------------------------------------- */
-const dates = [
-  {
-    startDate: new Date(2021, 1, 1, 10),
-    endDate: new Date(2021, 1, 1, 11),
-    title: "Cita de trabajo",
-  },
-  {
-    startDate: new Date(2021, 1, 1, 15),
-    endDate: new Date(2021, 1, 1, 15, 30),
-    title: "Cita con mi jefe",
-  },
-  {
-    startDate: new Date(2021, 1, 1, 20),
-    endDate: new Date(2021, 1, 1, 21),
-    title: "Cena",
-  },
-];
-const newAppointment = {
-  startDate: new Date(2021, 1 , 1, 19),
-  endDate: new Date(2021, 1, 1, 20, 30),
-};
 
-const areIntervalsOverlapping = require("date-fns/areIntervalsOverlapping");
+let rta = false;
+for (let index = 0; index < numbers.length; index++) {
+  const element = numbers[index];
+  if (element % 2 === 0) {
+    rta = true;
+    break;
+  }
+}
+console.log(rta);
 
-const isOverlap = (newDate) => {
-  return dates.some((date) => {
-      console.log(date);
-    return areIntervalsOverlapping(
-      { start: date.startDate, end: date.endDate},
-      { start: newDate.startDate, end: newDate.endDate }
-    );
-  });
-};
-console.log(isOverlap(newAppointment));
+const rta2 = numbers.some((item) => item % 2 === 0);
+console.log("rta2", rta2);
 
-/* --------------------------------------------------------------------------------- */
 const orders = [
   {
     customerName: "Nicolas",
@@ -60,6 +34,48 @@ const orders = [
     total: 240,
     delivered: true,
   },
+  {
+    customerName: "Nicolas",
+    total: 2322,
+    delivered: false,
+  },
 ];
-const rta2 = orders.some((item) => item.delivered);
-console.log("rta2", rta2);
+
+const rta3 = orders.some((item) => item.delivered);
+console.log("rta3", rta3);
+
+const dates = [
+  {
+    startDate: new Date(2021, 1, 1, 10),
+    endDate: new Date(2021, 1, 1, 11),
+    title: "Cita de trabajo",
+  },
+  {
+    startDate: new Date(2021, 1, 1, 15),
+    endDate: new Date(2021, 1, 1, 15, 30),
+    title: "Cita con mi jefe",
+  },
+  {
+    startDate: new Date(2021, 1, 1, 20),
+    endDate: new Date(2021, 1, 1, 21),
+    title: "Cena",
+  },
+];
+
+const newAppointment = {
+  startDate: new Date(2021, 1, 1, 8),
+  endDate: new Date(2021, 1, 1, 9, 30),
+};
+
+const areIntervalsOverlapping = require("date-fns/areIntervalsOverlapping");
+
+const isOverlap = (newDate) => {
+  return dates.some((date) => {
+    return areIntervalsOverlapping(
+      { start: date.startDate, end: date.endDate },
+      { start: newDate.startDate, end: newDate.endDate }
+    );
+  });
+};
+
+console.log("isOverlap", isOverlap(newAppointment));
